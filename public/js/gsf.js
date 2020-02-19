c40a84c4c33497d2ebe64ae0844e1caaf3a518c3
jmcasaubon <jmcasaubon@gmail.com>
2020-02-19 20:35:02 +0100

    Amélioration de l'apparence des blocs "programme" dans l'ajout/modif. de session + commentaire du code JS

--- public/js/gsf.js
+++ public/js/gsf.js
@@ -1,35 +1,38 @@
 $(document).ready(function() { // Une fois que le document (base.html.twig) HTML/CSS a bien été complètement chargé...
-    // add-collection-widget.js
+
+    // add-collection-widget.js : fonction permettant d'ajouter un nouveau bloc "programme" au sein d'une session (pour agrandir la collection)
     $('.add-another-collection-widget').click(function (e) {
         var list = $($(this).attr('data-list-selector'))
-        // Try to find the counter of the list or use the length of the list
+        // Récupération du nombre actuel d'élément "programme" dans la collection (à défaut, utilisation de la longueur de la collection)
         var counter = list.data('widget-counter') || list.children().length
+        // Récupération de l'identifiant de la session concernée, en cours de création/modification
         var session = list.data('session')
-        // grab the prototype template
+        // Extraction du prototype complet du champ (que l'on va adapter ci-dessous)
         var newWidget = list.attr('data-prototype')
-        // replace the "__name__" used in the id and name of the prototype
-        // with a number that's unique to "programmes"
-        // end name attribute looks like name="session[programmes][2]"
+        // Remplacement des séquences génériques "__name__" utilisées dans les parties "id" et "name" du prototype
+        // par un numéro unique au sein de la collection de "programmes" : ce numéro sera la valeur du compteur
+        // courant (équivalent à l'index du prochain champ, en cours d'ajout).
+        // Au final, l'attribut ressemblera à "session[programmes][n°]"
         newWidget = newWidget.replace(/__name__/g, counter)
+        // Ajout également des attributs personnalisés "class" et "value", qui n'apparaissent pas dans le prototype original 
         newWidget = newWidget.replace(/><input type="hidden"/, ' class="borders"><input type="hidden" value="'+session+'"')
-        // Increase the counter
+        // Incrément du compteur d'éléments et mise à jour de l'attribut correspondant
         counter++
-        // And store it, the length cannot be used if deleting widgets is allowed
         list.data('widget-counter', counter)
-
-        // create a new list element and add it to the list
-
+        // Création d'un nouvel élément (avec son bouton de suppression), et ajout à la fin de la liste des éléments existants
         var newElem = $(list.attr('data-widget-tags')).html(newWidget)
         addDeleteLink($(newElem).find('div.borders'))
         newElem.appendTo(list)
     });
 
+    // remove-collection-widget.js : fonction permettant de supprimer un bloc "programme" existant au sein d'une session
     $('.remove-collection-widget').find('div.borders').each(function() {
         addDeleteLink($(this))
     })
 
+    // fonction permettant l'ajout d'un bouton "Supprimer ce module" dans un bloc "programme", et d'enregistrer l'évenement "click" associé
     function addDeleteLink($moduleForm) {
-        var $removeFormButton = $('<button type="button" class="button flex-action">Supprimer ce module</button>');
+        var $removeFormButton = $('<div class="block"><button type="button" class="button">Supprimer ce module</button></div>');
         $moduleForm.append($removeFormButton)
     
         $removeFormButton.on('click', function(e) {
